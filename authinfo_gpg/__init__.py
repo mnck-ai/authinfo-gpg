119f65ca6cfd8b6a29babf66f2acdd637b92b8ed
Armin Monecke <github@mnck.ai>
2026-04-01 19:14:53 +0200

    fix: correct author email, bump to 0.1.1

--- authinfo_gpg/__init__.py
+++ authinfo_gpg/__init__.py
@@ -6,5 +6,5 @@ A simple Python library to read credentials from GPG-encrypted .authinfo files.
 
 from .core import AuthInfoGPG, AuthEntry, get_entry, find_gpg_binary
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["AuthInfoGPG", "AuthEntry", "get_entry", "find_gpg_binary"]
